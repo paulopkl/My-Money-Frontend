1680afecc1f13854ec3f406a274e11c6d6c9ead1
paulopkl <palmeida.ipms@gmail.com>
2021-08-14 12:07:08 -0300

    Finished deploy

--- src/main/consts.js
+++ src/main/consts.js
@@ -1,1 +1,4 @@
-export default { API_URL: 'http://localhost:3003/api', OAPI_URL: 'http://localhost:3003/oapi' }
+export default { 
+    API_URL: process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : 'http://localhost:3003/api', 
+    OAPI_URL: process.env.REACT_APP_OAPI_URL ? process.env.REACT_APP_OAPI_URL : 'http://localhost:3003/oapi' 
+}
